cf49fb430ee234c21ed4d2829dabaae4074ba3f2
Shirish Kadam <shirishkadam35@yahoo.com>
2017-03-30 00:05:15 +0530

    Added Camera Activity

--- app/src/main/java/com/alleviate/eyescan/MainActivity.java
+++ app/src/main/java/com/alleviate/eyescan/MainActivity.java
@@ -1,11 +1,14 @@
 package com.alleviate.eyescan;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +39,18 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EX_STORAGE);
             }
         }
+
+        Button camera_init = (Button) findViewById(R.id.init_camera);
+        camera_init.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent in = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(in);
+
+            }
+        });
+
     }
 
     @Override
